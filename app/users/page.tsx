52056ea6d3b04d94785cf3be9444596d4ab5afe8
Leonardo <leonardokbruksch@hotmail.com>
2023-08-25 16:57:20 +1000

    Error handling, nextjs default error page

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,6 +3,9 @@ import style from "./page.module.css";
 import UserCard from "@/components/UserCard/UserCard";
 
 export default async function Users() {
+
+    throw new Error('Shit has gone south.');
+
     const users = await prisma.user.findMany();
   
     return (
